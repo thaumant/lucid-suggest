f5fcf02dea885fe0a79b91d23e6c14b62cefda58
Ilia Schelokov <thaumant@gmail.com>
2020-06-20 14:40:26 +0300

    Remove tokenization stage from search benchmark

--- rust/core/benches/search.rs
+++ rust/core/benches/search.rs
@@ -3,17 +3,17 @@ use lucid_suggest_core::{Store, Record, search, lang, lexis};
 
 fn text_match_benchmark(criterion: &mut Criterion) {
     criterion.bench_function("search", |bench| {
-        let lang = lang::lang_english();
         let mut store = Store::new();
-        store.lang = Some(lang);
+        store.lang = Some(lang::lang_english());
         store.add(Record::new(10, "AA 1.5 Alkaline Batteries — Pack of 12", 10, &store.lang));
         store.add(Record::new(20, "Lightning to USB A Cable",               20, &store.lang));
         store.add(Record::new(30, "Electric Toothbrush",                    30, &store.lang));
         store.add(Record::new(40, "Vacuum Compression Storage Bags",        40, &store.lang));
 
+        let query = lexis::tokenize_query("compressing bag for clothes", &store.lang);
+        let query = query.to_ref();
+
         bench.iter(|| {
-            let query = lexis::tokenize_query("compressing bag for clothes", &store.lang);
-            let query = query.to_ref();
             search(&store, &query).collect::<Vec<_>>()
         })
     });
